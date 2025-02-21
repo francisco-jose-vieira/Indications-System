4edb665cad69d42c62a31441a84e3fdda2a8f358
francisco-josé-vieira <franciscojose2703@gmail.com>
2025-02-20 23:58:37 -0300

    Importando as fontes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+      <h1 className="text-4xl font-heading">njdsfdskldgsmklfdglmk~fgdsklç</h1>
     </main>
   );
 }
