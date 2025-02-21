2b67833ce00cff7c1230023b08aacc0b93c6c32d
francisco-josé-vieira <franciscojose2703@gmail.com>
2025-02-21 01:58:24 -0300

    Inputs concluídos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
-
+import { ArrowRight, Copy, Mail } from "lucide-react";
+import { Button } from "../components/button";
+import { IconButton } from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
 
 export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
-      <h1 className="text-4xl font-heading">njdsfdskldgsmklfdglmk~fgdsklç</h1>
+      <h1 className="text-4xl font-heading"></h1>
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+          <Mail className="size-5"/>
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   );
 }
